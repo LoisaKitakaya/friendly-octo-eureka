83565b8e0b33541a3bf74d37a23d6307b89b2dfe
Loisa Kitakaya <kitakayaloisa@gmail.com>
2025-03-03 14:43:07 +0300

    configured django-ninja

--- app/urls.py
+++ app/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from api.v1.api import api
 from django.urls import path
 from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = [
+    path("api/v1/", api.urls),
     path("admin/", admin.site.urls),
 ]
 
