568f480968a375de7a6e2dd9c1f063a02f5a4a86
Loisa Kitakaya <kitakayaloisa@gmail.com>
2025-03-03 19:46:06 +0300

    app update

--- orders/models.py
+++ orders/models.py
@@ -39,7 +39,7 @@ class Order(models.Model):
     total_price = models.DecimalField(
         max_digits=10,
         decimal_places=2,
-        default=0,
+        default=0, # type: ignore
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
